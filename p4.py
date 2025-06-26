5d4a6f666ce0ea20e1be791ffe143b76358d19a8
Khushiiiii22 <khushikumari62406@gmail.com>
2025-06-26 09:33:28 +0530

    Table

--- p4.py
+++ p4.py
@@ -4,4 +4,4 @@
 input_number = int(input('Enter a number to print its Math table: '))
 
 for i in range(1,11):
-    print('%2d * %2d = %d' % (input_number , i ,(input_number*i) ))
+    print('%2d * %2d = %3d' % (input_number , i ,(input_number*i) ))
