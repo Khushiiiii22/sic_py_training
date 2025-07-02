c42b6360a0d230edc17aad249541c12c92d45412
Khushiiiii22 <khushikumari62406@gmail.com>
2025-07-02 11:59:06 +0530

    .

---
+++ problem_solving/problem5.py
@@ -0,0 +1,17 @@
+# Orange partition program
+
+n = int(input('Enter the number of oranges'))
+oranges = list(map(int,input('Enter the size of oranges: ').split ()))
+
+
+k = 0
+pivot = oranges[-1]
+
+for i in range(n-1):
+    if oranges[i] <= pivot:
+        oranges[i] , oranges[k] = oranges[k] , oranges[i]
+        k += 1
+
+oranges[k] , pivot = pivot , oranges[k]
+
+print(' '.join(map(str, oranges))) 
